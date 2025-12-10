5127852aadea9da4c95eb0c8398755fea0b505fc
Misael Borges <mizaelborges2011@gmail.com>
2025-12-10 13:53:03 -0300

    projeto criado

---
+++ src/test/java/com/fintech/wallet_service/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.wallet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
