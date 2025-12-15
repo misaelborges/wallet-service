af26ef92af6f0cf4505bc0341f439e173cd6255e
Misael Borges <mizaelborges2011@gmail.com>
2025-12-15 13:10:16 -0300

    feat: cria ContaService

---
+++ src/main/java/com/fintech/wallet_service/service/ContaService.java
@@ -0,0 +1,51 @@
+package com.fintech.wallet_service.service;
+
+import com.fintech.wallet_service.config.mapper.ContaMapper;
+import com.fintech.wallet_service.config.service.UsuarioServiceCliente;
+import com.fintech.wallet_service.dto.ContaRequestDTO;
+import com.fintech.wallet_service.dto.ContaResponseDTO;
+import com.fintech.wallet_service.entity.Conta;
+import com.fintech.wallet_service.repository.ContaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+@Service
+public class ContaService {
+
+    private final ContaRepository contaRepository;
+    private final ContaMapper contaMapper;
+    private final UsuarioServiceCliente usuarioServiceCliente;
+
+    public ContaService(ContaRepository contaRepository, ContaMapper contaMapper, UsuarioServiceCliente usuarioServiceCliente) {
+        this.contaRepository = contaRepository;
+        this.contaMapper = contaMapper;
+        this.usuarioServiceCliente = usuarioServiceCliente;
+    }
+
+    @Transactional
+    public ContaResponseDTO criarConta(ContaRequestDTO contaRequestDTO) {
+        if (!usuarioServiceCliente.usuarioExiste(contaRequestDTO.usuarioId())) {
+            throw new RuntimeException("Usuario não foi contrado");
+        }
+        Conta conta = contaMapper.toEntity(contaRequestDTO);
+        conta.setNumeroConta(gerarNumeroConta());
+        conta.setSaldo(BigDecimal.ZERO);
+        conta.setAtiva(true);
+        Conta contaSalvar = contaRepository.save(conta);
+        return contaMapper.toResponseDTO(contaSalvar);
+    }
+
+    private String gerarNumeroConta() {
+        Random random = new Random();
+        StringBuilder numero = new StringBuilder("ACC-");
+
+        for (int i = 0; i < 10; i++) {
+            numero.append(random.nextInt(10));
+        }
+
+        return numero.toString();
+    }
+}
