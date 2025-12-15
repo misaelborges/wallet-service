41fc520c3e65ccfd083109f0b50c9fa315408d81
Misael Borges <mizaelborges2011@gmail.com>
2025-12-15 13:56:07 -0300

    feat: cria rota para buscar as Contas de um usuario

--- src/main/java/com/fintech/wallet_service/controller/ContaController.java
+++ src/main/java/com/fintech/wallet_service/controller/ContaController.java
@@ -2,11 +2,14 @@ package com.fintech.wallet_service.controller;
 
 import com.fintech.wallet_service.dto.ContaRequestDTO;
 import com.fintech.wallet_service.dto.ContaResponseDTO;
+import com.fintech.wallet_service.dto.ContaResumoResponseDTO;
 import com.fintech.wallet_service.service.ContaService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/carteiras")
@@ -28,5 +31,11 @@ public class ContaController {
     public ResponseEntity<ContaResponseDTO> buscarCarteiraPorId(@PathVariable Long id) {
         ContaResponseDTO contaResponseDTO = contaService.buscarCarteiraPorId(id);
         return ResponseEntity.status(HttpStatus.OK).body(contaResponseDTO);
+    }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<ContaResumoResponseDTO>>listarCarteirasUsuario(@PathVariable Long usuarioId) {
+        List<ContaResumoResponseDTO> contaResponseDTOS = contaService.listarCarteirasUsuario(usuarioId);
+        return ResponseEntity.status(HttpStatus.OK).body(contaResponseDTOS);
     }
 }
