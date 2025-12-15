675789a5d419455fb6f9bf152b8687b38d83fbb6
Misael Borges <mizaelborges2011@gmail.com>
2025-12-15 15:05:01 -0300

    feat: cria metodo para converte Conta em SaldoResponseDTO

--- src/main/java/com/fintech/wallet_service/config/mapper/ContaMapper.java
+++ src/main/java/com/fintech/wallet_service/config/mapper/ContaMapper.java
@@ -3,8 +3,10 @@ package com.fintech.wallet_service.config.mapper;
 import com.fintech.wallet_service.dto.ContaRequestDTO;
 import com.fintech.wallet_service.dto.ContaResponseDTO;
 import com.fintech.wallet_service.dto.ContaResumoResponseDTO;
+import com.fintech.wallet_service.dto.SaldoResponseDTO;
 import com.fintech.wallet_service.entity.Conta;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -14,4 +16,7 @@ public interface ContaMapper {
     Conta toEntity(ContaRequestDTO contaRequestDTO);
     ContaResponseDTO toResponseDTO(Conta conta);
     List<ContaResumoResponseDTO> toResponseDTO(List<Conta> conta);
+
+    @Mapping(target = "dataConsulta", expression = "java(OffsetDateTime.now())")
+    SaldoResponseDTO toSaldoResponseDTO(Conta conta);
 }
