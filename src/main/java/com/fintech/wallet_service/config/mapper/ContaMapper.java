44f97edf194b71164917520e607cd0ba8b66319f
Misael Borges <mizaelborges2011@gmail.com>
2025-12-15 13:55:11 -0300

    feat: converte uma Lista de Conta em uma Lista de ContaResumoResponseDTO

--- src/main/java/com/fintech/wallet_service/config/mapper/ContaMapper.java
+++ src/main/java/com/fintech/wallet_service/config/mapper/ContaMapper.java
@@ -2,12 +2,16 @@ package com.fintech.wallet_service.config.mapper;
 
 import com.fintech.wallet_service.dto.ContaRequestDTO;
 import com.fintech.wallet_service.dto.ContaResponseDTO;
+import com.fintech.wallet_service.dto.ContaResumoResponseDTO;
 import com.fintech.wallet_service.entity.Conta;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ContaMapper {
 
     Conta toEntity(ContaRequestDTO contaRequestDTO);
     ContaResponseDTO toResponseDTO(Conta conta);
+    List<ContaResumoResponseDTO> toResponseDTO(List<Conta> conta);
 }
